15ed846987a58ee91c2a2509d52798a2e4873f6b
Alexey Abadzhev <alexeyabadzhev@yandex.ru>
2026-03-11 10:12:44 +0300

    version 1.0

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
-}
+   const discount = 1 - (purchase.discount / 100);
+   return purchase.sale_price * purchase.quantity * discount;
+};
 
 /**
  * Функция для расчета бонусов
@@ -17,6 +19,18 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
+
+    if (index === 0) {
+        return 15;
+    }
+    if (index === 1 || index === 2) {
+        return 10;
+    }
+    if (index === 3) {
+        return 5;
+    }
+    return 0;
 }
 
 /**
@@ -27,18 +41,88 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
+    if (!data
+        || !Array.isArray(data.sellers)
+        || !Array.isArray(data.products)
+        || !Array.isArray(data.purchase_records)
+        || data.sellers.length === 0
+        || data.products.length === 0
+        || data.purchase_records.length === 0
+    ) {
+        console.error('Входящие данные не верны!')
+    }
 
     // @TODO: Проверка наличия опций
+    const { calculateBonus, calculateRevenue } = options;
+    if (!calculateBonus || !calculateRevenue) {
+        console.error('Не достаточно обработчиков!')
+    }
 
     // @TODO: Подготовка промежуточных данных для сбора статистики
+    const middleStatistic = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
+    }));
 
     // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerObject = Object.fromEntries(middleStatistic.map(seller => [seller.id, seller]));
+    const productObject = Object.fromEntries(data.products.map(product => [product.sku, product]));
 
     // @TODO: Расчет выручки и прибыли для каждого продавца
+    data.purchase_records.forEach(item => {
+        const seller = sellerObject[item.seller_id];
+        seller.sales_count += 1;
+        seller.revenue += item.total_amount;
+
+        let totalCost = 0;
+        let totalRevenue = 0;
+
+        item.items.forEach(item => {
+            const product = productObject[item.sku];
+            const cost = product.purchase_price * item.quantity;
+            const revenue = calculateRevenue(item, product);
+            const profit = revenue - cost;
+
+            totalCost += cost;
+            totalRevenue += revenue;
+
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0;
+            }
+
+            seller.products_sold[item.sku] += item.quantity;
+        });
+
+        seller.profit += totalRevenue - totalCost;
+    });
 
     // @TODO: Сортировка продавцов по прибыли
+    middleStatistic.sort((a,b) => b.profit - a.profit);
 
     // @TODO: Назначение премий на основе ранжирования
+    const summary = middleStatistic.length;
+    middleStatistic.forEach((seller, index) => {
+        const sellerBonus = calculateBonus(index, summary, seller);
+        seller.bonus = seller.profit * sellerBonus / 100;
+
+        seller.top_products = Object.entries(seller.products_sold)
+            .map(([sku, quantity]) => ({sku, quantity}))
+            .sort((a, b) => b.quantity - a.quantity)
+            .slice(0, 10);
+    });
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
+    return middleStatistic.map(seller => ({
+        seller_id: seller.id,
+        name: seller.name,
+        revenue: +seller.revenue.toFixed(2),
+        profit: +seller.profit.toFixed(2),
+        sales_count: seller.sales_count,
+        top_products: seller.top_products,
+        bonus: +seller.bonus.toFixed(2)
+    }));
 }
